5e0a74a8cdb058728d4b3dc6b4b6f40bf677f56f
AzureFirehawk <madison.thomas094@gmail.com>
2025-06-10 11:10:06 -0500

    updated import pathing

--- scripts/FavoritesUI.mjs
+++ scripts/FavoritesUI.mjs
@@ -1,4 +1,4 @@
-import { isFavorite, addFavorite, removeFavorite } from "./Favorites.mjs";
+import { isFavorite, addFavorite, removeFavorite } from "./FavoritesList.mjs";
 
 function handleAddFavorite(apodData) {
     const result = addFavorite(apodData);
