8bdee7285c84bf25acd2557ad4b171ae11e0b6d7
AzureFirehawk <madison.thomas094@gmail.com>
2025-06-10 11:21:58 -0500

    script order fix

--- scripts/display.js
+++ scripts/display.js
@@ -4,13 +4,6 @@ import { renderCalendar } from "./Calendar.mjs";
 
 loadHeaderFooter();
 
-document.querySelector("h2").innerHTML = "Astronomy Picture of the Day | "
-    + new Date(date).toLocaleDateString("en-US", {
-    month: "short",
-    day: "numeric",
-    year: "numeric"
-});
-
 function getDateFromQuery() {
     const params = new URLSearchParams(window.location.search);
     return params.get('date') || undefined;
@@ -19,5 +12,11 @@ function getDateFromQuery() {
 const date = getDateFromQuery();
 showApodInfo(date);
 
+document.querySelector("h2").innerHTML = "Astronomy Picture of the Day | "
+    + new Date(date).toLocaleDateString("en-US", {
+    month: "short",
+    day: "numeric",
+    year: "numeric"
+});
 const today = new Date();
 renderCalendar(today.getMonth(), today.getFullYear());
