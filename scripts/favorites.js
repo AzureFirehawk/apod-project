30e4de56696d601bc863afe5497497a80792f0d6
AzureFirehawk <madison.thomas094@gmail.com>
2025-06-10 11:43:22 -0500

    import pathting for favorites storage, template update to save favorites date as formated text

--- scripts/favorites.js
+++ scripts/favorites.js
@@ -1,15 +1,21 @@
 import { loadHeaderFooter } from "./utils.mjs";
-import { getFavorites } from "./FavoritesList.mjs";
+import { getFavorites } from "./FavoritesStorage.mjs";
 
 loadHeaderFooter();
 
 function favoriteCardTemplate(favorite) {
     const { date, title, url } = favorite;
+    const formatDate = new Date(date).toLocaleDateString("en-US", {
+        month: "short", day: "numeric", year: "numeric"
+    })
     return `
         <div class="favorite-card">
-        <img src="${url}" alt="${title}">
-        <h3>${title}</h3>
-        <p>${date}</p>
+            <a href="result.html?date=${date}">
+                <img src="${url}" alt="${title}">
+                <h3>${title}</h3>
+                <p>${formatDate}</p>
+            </a>
+            <button class="remove-favorite" data-date="${date}">Remove</button>
         </div>
     `;
 }
