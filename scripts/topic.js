ca8ef72e416baa5bb0cc0d7f3f537c5033a431fa
AzureFirehawk <madison.thomas094@gmail.com>
2025-06-13 13:07:57 -0500

    cate formating and card creation

--- scripts/topic.js
+++ scripts/topic.js
@@ -7,3 +7,23 @@ function getTopicFromURL() {
     const urlParams = new URLSearchParams(window.location.search);
     return urlParams.get("topic") || "";
 }
+
+function formatDate(date) {
+    return new Date(date).toISOString().split("T")[0];
+};
+
+function getDateRange(daysBack = 7) {
+    const end = new Date();
+    const start = new Date();
+    start.setDate(start.getDate() - daysBack);
+    return {
+        start: formatDate(start),
+        end: formatDate(end)
+    };
+}
+
+function createApodCard(apod) {
+    const card = document.createElement("div");
+    card.classList.add("apod-card");
+
+}
